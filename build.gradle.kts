70c92c80325c4e1a270ff1c714881fd28e58afd1
xingmin1 <z_xmin@foxmail.com>
2025-05-11 17:39:44 +0800

    chore: 更新.gitignore以排除IDE相关文件，升级Gradle插件版本至8.9.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.9.1" apply false
-    id("com.android.library") version "8.9.1" apply false
+    id("com.android.application") version "8.9.2" apply false
+    id("com.android.library") version "8.9.2" apply false
     alias(libs.plugins.kotlin.android) apply false
     id("com.google.dagger.hilt.android") version "2.48" apply false
     alias(libs.plugins.kotlin.compose) apply false
